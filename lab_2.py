1199d26b96b6c1f82473ee6d94115febf0d9e9c6
rostmoguchiy@gmail.com <3831200011nnn>
2019-03-17 10:09:50 +0200

    hello

---
+++ lab_2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+# listBox = Listbox(root, width=15, height=8)
+# listBox.pack()
+# for i in [1,2,3,4,5,6]:
+#     listBox.insert(0, i)
+#
+# print(listBox.get())
+# root.mainloop()
