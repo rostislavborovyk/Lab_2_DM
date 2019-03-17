6a91d6549b0500e35bf82479308a7e2af67a2f95
rostmoguchiy@gmail.com <3831200011nnn>
2019-03-17 10:32:19 +0200

    hello

--- lab_2.py
+++ lab_2.py
@@ -1,10 +1,34 @@
 from tkinter import *
 
+
+def window2():
+    pass
+
+
+def window3():
+    pass
+
+
+def window4():
+    pass
+
+
 root = Tk()
-# listBox = Listbox(root, width=15, height=8)
-# listBox.pack()
-# for i in [1,2,3,4,5,6]:
-#     listBox.insert(0, i)
-#
-# print(listBox.get())
-# root.mainloop()
+# root.geometry('600x400')
+
+mainMenu = Menu(root)
+root.config(menu=mainMenu)
+
+win2 = Menu(mainMenu, tearoff=0)
+mainMenu.add_cascade(menu=win2, label='Вікно 2')
+win2.add_command(label='Відкрити', command=window2)
+
+win3 = Menu(mainMenu, tearoff=0)
+mainMenu.add_cascade(menu=win3, label='Вікно 3')
+win3.add_command(label='Відкрити', command=window3)
+
+win4 = Menu(mainMenu, tearoff=0)
+mainMenu.add_cascade(menu=win4, label='Вікно 4')
+win4.add_command(label='Відкрити', command=window4)
+
+root.mainloop()
